ea575601ae3121f81460f7e82efba8b4c8e328d9
Sean Kleinjung <sean@thrashplay.com>
2021-11-01 14:42:24 +0000

    Add security settings to electron config.

--- public/electron.js
+++ public/electron.js
@@ -1,36 +1,68 @@
 const path = require('path')
 
 const { app, BrowserWindow } = require('electron')
-const isDev = require('electron-is-dev')
 
 function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
     },
   })
 
-  // and load the index.html of the app.
-  // win.loadFile("index.html");
-  win.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  )
+  // In production, set the initial browser path to the local bundle generated
+  // by the Create React App build process.
+  // In development, set it to localhost to allow live/hot-reloading.
+  const appURL = app.isPackaged
+    ? url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+    : 'http://localhost:3000'
+  mainWindow.loadURL(appURL)
 
-  // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' })
+  // Automatically open Chrome's DevTools in development mode.
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools()
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+// Setup a local proxy to adjust the paths of requested files when loading
+// them from the local production bundle (e.g.: local fonts, etc...).
+function setupLocalFilesNormalizerProxy () {
+  protocol.registerHttpProtocol(
+    'file',
+    (request, callback) => {
+      const url = request.url.substr(8)
+      // eslint-disable-next-line node/no-callback-literal
+      callback({ path: path.normalize(`${__dirname}/${url}`) })
+    },
+    (error) => {
+      // eslint-disable-next-line no-console
+      if (error) console.error('Failed to register protocol')
+    }
+  )
+}
+
+// This method will be called when Electron has finished its initialization and
+// is ready to create the browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow()
+  setupLocalFilesNormalizerProxy()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -41,8 +73,16 @@ app.on('window-all-closed', () => {
   }
 })
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+// If your app has no need to navigate or only needs to navigate to known pages,
+// it is a good idea to limit navigation outright to that known scope,
+// disallowing any other kinds of navigation.
+const allowedNavigationDestinations = 'https://some-url.thrashplay.com'
+app.on('web-contents-created', (_event, contents) => {
+  contents.on('will-navigate', (event, navigationUrl) => {
+    const parsedUrl = new URL(navigationUrl)
+
+    if (!allowedNavigationDestinations.includes(parsedUrl.origin)) {
+      event.preventDefault()
+    }
+  })
 })
