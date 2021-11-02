674f3396f6b69f99ed2d12da925515bcb23761c0
Sean Kleinjung <sean@thrashplay.com>
2021-11-02 11:52:22 -0500

    Add navigation and exit functionality to title screen.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,33 @@
 import './app.css'
 import { loadFonts } from 'fonts'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
+import { DungeonScreen } from './dungeon-screen'
 import { TitleScreen } from './title-screen'
+
+export type ScreenName = 'dungeon' | 'exit' | 'game-over' | 'title'
 
 function App () {
   const [ready, setReady] = useState(false)
+  const [activeScreen, setActiveScreen] = useState<ScreenName>('title')
 
   loadFonts().then(() => setReady(true))
 
-  return ready ? (
-    <TitleScreen />
-  ) : <div>Loading...</div>
+  const handleExit = useCallback(() => {
+    (window as any).ipcRenderer.send('exit')
+  }, [])
+
+  const getActiveScreen = () => {
+    switch (activeScreen) {
+      case 'dungeon':
+        return <DungeonScreen />
+
+      default:
+        return <TitleScreen exit={handleExit} navigateTo={setActiveScreen} />
+    }
+  }
+
+  return ready ? getActiveScreen() : <div>Loading...</div>
 }
 
 export default App
--- src/components/title-screen.tsx
+++ src/components/title-screen.tsx
@@ -1,6 +1,16 @@
 import { findIndex, map, noop } from 'lodash/fp'
 import React, { useCallback, useState } from 'react'
+
+import { ScreenName } from './app'
 import './title-screen.css'
+
+export interface TitleScreenProps {
+  /** called when the user requests to exit the app */
+  exit: () => void
+
+  /** function that allows inter-screen navigation */
+  navigateTo: (screen: ScreenName) => void
+}
 
 interface MenuProps {
   /** the item to select when the list first renders */
@@ -100,15 +110,22 @@ const Menu = ({
   )
 }
 
-export const TitleScreen = () => {
+export const TitleScreen = ({ exit, navigateTo }: TitleScreenProps) => {
   return (
     <>
       <h1>Shift</h1>
       <Menu
         items={['New Game', 'Exit']}
         onSelectionConfirmed={(item) => {
-          // eslint-disable-next-line no-console
-          console.log('confirmed:', item)
+          switch (item) {
+            case 'New Game':
+              navigateTo('dungeon')
+              break
+
+            case 'Exit':
+              exit()
+              break
+          }
         }}
       />
     </>
