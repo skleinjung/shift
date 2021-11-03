a1c529356f7dded4f07343b06012887e4e8966fb
Sean Kleinjung <sean@thrashplay.com>
2021-11-03 14:58:23 -0500

    Add useKeyHandler hook for working with global key events.

---
+++ src/hooks/use-key-handler.ts
@@ -0,0 +1,22 @@
+import { noop } from 'lodash/fp'
+import { useCallback, useEffect } from 'react'
+
+/**
+ * Registers a 'keydown' handler on the window, and unregisters it when the component is unmounted.
+ * The handler will dispatch to one of a set of key handlers whenever a key is pressed. The handlers
+ * are provided via the `handlerMap`. The map keys are the key values (i.e. `event.key`), and the
+ * value is a no-arg function that will be called when that key is pressed.
+ */
+export const useKeyHandler = (handlerMap: Record<string, () => void>) => {
+  const handler = useCallback((event: KeyboardEvent) => {
+    const keyHandler = handlerMap[event.key] ?? noop
+    keyHandler()
+  }, [handlerMap])
+
+  useEffect(() => {
+    window.addEventListener('keydown', handler)
+    return () => {
+      window.removeEventListener('keydown', handler)
+    }
+  }, [handler])
+}
