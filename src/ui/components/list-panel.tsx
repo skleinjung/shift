79895822325c8d96a5c7ddf43fdf4255ed78f8b8
Sean Kleinjung <sean@thrashplay.com>
2021-11-06 11:01:01 -0500

    Add title and scrolling to ListPanel.

--- src/ui/components/list-panel.tsx
+++ src/ui/components/list-panel.tsx
@@ -53,6 +53,7 @@ export const ListPanel = ({
   items,
   onItemConsidered = noop,
   onItemSelected = noop,
+  title,
   ...rest
 }: ListPanelProps) => {
   const [selectedIndex, setSelectedIndex] = useState(0)
@@ -110,13 +111,13 @@ export const ListPanel = ({
     ])
 
     return (
-      <div key={getId(item)}
+      <li key={getId(item)}
         className={classes}
         onClick={handleClick(index)}
       >
         <div className="list-panel-left">{getLeftContent(item)}</div>
         {rightContent && <div className="list-panel-right">{rightContent}</div> }
-      </div>
+      </li>
     )
   }
 
@@ -125,7 +126,10 @@ export const ListPanel = ({
       active={active}
       onKeyDown={handleKeyDown}
     >
-      {mapS(items, createRow)}
+      {title && <h2 className="list-panel-title">{title}</h2>}
+      <ul className="list-panel">
+        {mapS(items, createRow)}
+      </ul>
     </Panel>
   )
 }
