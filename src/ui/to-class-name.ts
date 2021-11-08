b67ae80af1c033474a1288210c1f3ca0cc17e9c3
Sean Kleinjung <sean@thrashplay.com>
2021-11-08 12:50:51 -0600

    Add generic 'modal' component.

---
+++ src/ui/to-class-name.ts
@@ -0,0 +1,14 @@
+import { castArray, join } from 'lodash/fp'
+
+export type WithExtraClasses = {
+  /** additional CSS classes to apply to the 'ul' content element */
+  classes?: string | string[]
+}
+
+/**
+ * Creates a standard `className` attribute by combining optional extra classes (as an array or single
+ * string) with an optional set of fixed, static classes.
+ */
+export const toClassName = (extraClasses: string | string[] | undefined, ...staticClasses: string[]) => {
+  return join(' ', [...staticClasses, ...castArray(extraClasses)])
+}
