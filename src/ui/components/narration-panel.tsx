9ea3bd7ee480c722c8db36acd8eb0d78b4f0a255
Sean Kleinjung <sean@thrashplay.com>
2021-11-11 21:41:00 -0600

    Add text fade in to narration dialog.

--- src/ui/components/narration-panel.tsx
+++ src/ui/components/narration-panel.tsx
@@ -1,7 +1,7 @@
 import './narration-panel.css'
 
 import { NarrationUnit } from 'engine/events'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useKeyHandler } from 'ui/hooks/use-key-handler'
 import { getKeyMap } from 'ui/key-map'
 import { toClassName, WithExtraClasses } from 'ui/to-class-name'
@@ -14,23 +14,42 @@ export type NarrationPanelProps = WithExtraClasses & Omit<PanelProps, 'className
 
   /** callback invoked when the narration has been fully displayed */
   onComplete: () => void
+
+  /** delay in ms between each character */
+  textDelay?: number
 }
 
 export const NarrationPanel = ({
   classes = [],
   content,
   onComplete,
+  textDelay = 13,
   ...panelProps
 }: NarrationPanelProps) => {
   const [contentIndex, setContentIndex] = useState(0)
+  const [currentCharacter, setCurrentCharacter] = useState(1)
 
   const handleNextPage = useCallback(() => {
     if (contentIndex >= content.length - 1) {
       onComplete()
     } else {
+      setCurrentCharacter(1)
       setContentIndex((current) => current + 1)
     }
   }, [content, contentIndex, onComplete])
+
+  useEffect(() => {
+    let timeout: ReturnType<typeof setTimeout> | undefined
+    if (content[contentIndex]?.message?.length > currentCharacter) {
+      timeout = setTimeout(() => setCurrentCharacter((current) => current + 1), textDelay)
+    }
+
+    return () => {
+      if (timeout !== undefined) {
+        clearTimeout(timeout)
+      }
+    }
+  }, [content, contentIndex, currentCharacter, textDelay])
 
   const keyMap = getKeyMap()
   const handleKeyDown = useKeyHandler({
@@ -39,6 +58,10 @@ export const NarrationPanel = ({
 
   const lastPage = contentIndex >= content.length - 1
 
+  const getText = () => {
+    return content[contentIndex].message.substring(0, currentCharacter)
+  }
+
   return contentIndex >= content.length ? null : (
     <Panel
       {...panelProps}
@@ -46,7 +69,7 @@ export const NarrationPanel = ({
       onKeyDown={handleKeyDown}
       title={content[contentIndex].speaker}
     >
-      <div className="narration-panel-message">{content[contentIndex].message}</div>
+      <div className="narration-panel-message">{getText()}</div>
       <div className="narration-panel-controls" onClick={handleNextPage}>{lastPage ? '(close)' : '(more)'}</div>
     </Panel>
   )
