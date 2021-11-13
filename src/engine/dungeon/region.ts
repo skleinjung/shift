c642084a773c99074f5d6d11f7257ef73998f73d
Sean Kleinjung <sean@thrashplay.com>
2021-11-13 15:27:57 -0600

    Create dungeons with forest tiles instead of cave. (WIP)

--- src/engine/dungeon/region.ts
+++ src/engine/dungeon/region.ts
@@ -1,5 +1,6 @@
 import { ExpeditionMap } from 'engine/map/map'
-import { TerrainTypes } from 'engine/terrain-db'
+import { random } from 'engine/random'
+import { TerrainType, TerrainTypes } from 'engine/terrain-db'
 
 export interface CreatureSpawn {
   type: string
@@ -12,6 +13,19 @@ export const RegionTypes = [
   'tunnel',
 ] as const
 export type RegionTypeName = (typeof RegionTypes)[number]
+
+export type TerrainTypeProviderFunction = () => TerrainType
+export interface TerrainTypeProvider {
+  door: TerrainTypeProviderFunction
+  floor: TerrainTypeProviderFunction
+  wall: TerrainTypeProviderFunction
+}
+
+const defaultTerrainTypes: TerrainTypeProvider = {
+  door: () => TerrainTypes.door,
+  floor: () => TerrainTypes.floor,
+  wall: () => TerrainTypes.wall,
+}
 
 /**
  * A contiguous area of a dungeon with similar characteristics, such as a room or tunnel.
@@ -37,18 +51,22 @@ export class Region {
 
   public readonly creatures: CreatureSpawn[] = []
 
-  constructor ({ left, top, width, height, type = 'room' }: {
+  public readonly terrainTypes: TerrainTypeProvider
+
+  constructor ({ left, top, width, height, type = 'room', terrainTypes = defaultTerrainTypes }: {
     left: number
     top: number
     width: number
     height: number
     type?: RegionTypeName
+    terrainTypes?: TerrainTypeProvider
   }) {
     this.left = left
     this.top = top
     this.width = width
     this.height = height
     this.type = type
+    this.terrainTypes = terrainTypes
   }
 
   /** right extent of the region */
@@ -89,11 +107,13 @@ export class Region {
     // create floors
     for (let y = this.top; y <= this.bottom; y++) {
       for (let x = this.left; x <= this.right; x++) {
-        if (this.type === 'tunnel' && map.getTerrain(x, y) === TerrainTypes.wall) {
-          const terrain = Math.random() < 0.35 ? TerrainTypes.door : TerrainTypes.floor
+        if (this.type === 'tunnel' && map.getTerrain(x, y) === this.terrainTypes.wall()) {
+          const terrain = Math.random() < 0.35
+            ? this.terrainTypes.door()
+            : this.terrainTypes.floor()
           map.setTerrain(x, y, terrain)
         } else {
-          map.setTerrain(x, y, TerrainTypes.floor)
+          map.setTerrain(x, y, this.terrainTypes.floor())
         }
       }
     }
@@ -101,23 +121,66 @@ export class Region {
     // create vertical walls
     for (let y = this.top - 1; y <= this.bottom + 1; y++) {
       if (map.getTerrain(this.left - 1, y) === TerrainTypes.default) {
-        map.setTerrain(this.left - 1, y, TerrainTypes.wall)
+        map.setTerrain(this.left - 1, y, this.terrainTypes.wall())
       }
 
       if (map.getTerrain(this.right + 1, y) === TerrainTypes.default) {
-        map.setTerrain(this.right + 1, y, TerrainTypes.wall)
+        map.setTerrain(this.right + 1, y, this.terrainTypes.wall())
       }
     }
 
     // create horizontal walls
     for (let x = this.left - 1; x <= this.right + 1; x++) {
       if (map.getTerrain(x, this.top - 1) === TerrainTypes.default) {
-        map.setTerrain(x, this.top - 1, TerrainTypes.wall)
+        map.setTerrain(x, this.top - 1, this.terrainTypes.wall())
       }
 
       if (map.getTerrain(x, this.bottom + 1) === TerrainTypes.default) {
-        map.setTerrain(x, this.bottom + 1, TerrainTypes.wall)
+        map.setTerrain(x, this.bottom + 1, this.terrainTypes.wall())
       }
     }
+  }
+}
+
+const ForestTerrainTypes: TerrainTypeProvider = {
+  door: () => TerrainTypes.brambles,
+  floor: () => {
+    switch (random(1, 3)) {
+      case 1:
+        return TerrainTypes.light_brush_1
+      case 2:
+        return TerrainTypes.light_brush_2
+      default:
+        return TerrainTypes.light_brush_3
+    }
+  },
+  wall: () => TerrainTypes.heavy_brush,
+}
+
+const ForestPathTerrainTypes: TerrainTypeProvider = {
+  door: () => TerrainTypes.brambles,
+  floor: () => TerrainTypes.path,
+  wall: () => TerrainTypes.heavy_brush,
+}
+
+export class ForestClearing extends Region {
+  constructor ({ left, top, width, height }: {
+    left: number
+    top: number
+    width: number
+    height: number
+  }) {
+    super({ left, top, width, height, terrainTypes: ForestTerrainTypes })
+  }
+}
+
+export class ForestPath extends Region {
+  constructor ({ left, top, width, height }: {
+    left: number
+    top: number
+    width: number
+    height: number
+  }) {
+    super({ left, top, width, height, type: 'tunnel', terrainTypes: ForestPathTerrainTypes })
   }
 }
