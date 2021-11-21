692fc389bcbe54263392beb4349af3ce1ca84737
Sean Kleinjung <sean@thrashplay.com>
2021-11-20 22:42:00 -0600

    Add movement commands.

--- src/engine/commands/index.ts
+++ src/engine/commands/index.ts
@@ -1,7 +1,17 @@
 import { Command } from 'engine/types'
 
 import { help } from './help'
+import { east, move, north, south, west } from './movement'
 
 export const Commands: { [k: string]: Command } = {
+  e: east,
+  east,
   help,
+  move,
+  n: north,
+  north,
+  s: south,
+  south,
+  w: west,
+  west,
 }
